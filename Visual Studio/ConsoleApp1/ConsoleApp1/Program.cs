8773d86db0b56b11e7d4b7169c72612b5c9b4009
Wagner Portela de Assis <wpassis@yahoo.com.br>
2022-10-05 16:12:23 -0300

    ConsoleApp1

--- Visual Studio/ConsoleApp1/ConsoleApp1/Program.cs
+++ Visual Studio/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Teste Console");
+﻿namespace ConsoleApp1
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teste Console");
+            Console.WriteLine("Teste Console2");
+            Console.WriteLine("Meu Nome");
+            var name = Console.ReadLine();
+            var currentDate = DateTime.Now;
+            Console.WriteLine($"{Environment.NewLine}Olá, {name}, sobre {currentDate:d} at {currentDate:t}!");
+            Console.Write($"{Environment.NewLine}Pressione qualquer tecla para sair...");
+            Console.ReadKey(true);
+        }
+    }
+}
