77a4f45ce9c122441a79d86c26a8c8f512060e17
Wagner Portela de Assis <wpassis@yahoo.com.br>
2022-10-06 14:33:34 -0300

    * Criar uma biblioteca de classes .NET usando o Visual Studio

---
+++ Visual Studio/ClassLibraryProjects/StringLibrary/Class1.cs
@@ -0,0 +1,22 @@
+﻿namespace UtilityLibraries;
+
+public static class StringLibrary
+{
+    public static bool StartsWithUpper(this string? str)
+    {
+        if (string.IsNullOrWhiteSpace(str))
+            return false;
+
+        char ch = str[0];
+        return char.IsUpper(ch);
+    }
+
+
+    public static void minhaMsg(string msgStr)
+    {
+        Console.WriteLine("teste com parametro passado: " + msgStr);
+        return ; 
+    }
+
+}
+
