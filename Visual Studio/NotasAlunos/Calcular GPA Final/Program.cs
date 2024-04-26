95dfc9f9e489ac7f5206c4a1593f9d5f9ea08ba6
Wagner Portela de Assis <wpassis@yahoo.com.br>
2024-04-26 16:30:56 -0300

    calcular gpa final dos alunos

---
+++ Visual Studio/NotasAlunos/Calcular GPA Final/Program.cs
@@ -0,0 +1,86 @@
+﻿// See https://aka.ms/new-console-template for more information
+// calcular o gpa final do aluno[
+
+string studentName = "Sophia Johnson";
+string course1Name = "English 101";
+string course2Name = "Algebra 101";
+string course3Name = "Biology 101";
+string course4Name = "Computer Science I";
+string course5Name = "Psychology 101";
+
+int course1Credit = 3;
+int course2Credit = 3;
+int course3Credit = 4;
+int course4Credit = 4;
+int course5Credit = 3;
+
+int gradeA = 4;
+int gradeB = 3;
+
+int course1Grade = gradeA;
+int course2Grade = gradeB;
+int course3Grade = gradeB;
+int course4Grade = gradeB;
+int course5Grade = gradeA;
+
+Console.WriteLine($"{course1Name} {course1Grade} {course1Credit}");
+Console.WriteLine($"{course2Name} {course2Grade} {course2Credit}");
+Console.WriteLine($"{course3Name} {course3Grade} {course3Credit}");
+Console.WriteLine($"{course4Name} {course4Grade} {course4Credit}");
+Console.WriteLine($"{course5Name} {course5Grade} {course5Credit}");
+
+// Calcular as somas de horas de crédito e pontos de nota
+
+int totalCreditHours = 0;
+
+totalCreditHours += course1Credit;
+totalCreditHours += course2Credit;
+totalCreditHours += course3Credit;
+totalCreditHours += course4Credit;
+totalCreditHours += course5Credit;
+
+int totalGradePoints = 0;
+
+totalGradePoints += course1Credit * course1Grade;
+totalGradePoints += course2Credit * course2Grade;
+totalGradePoints += course3Credit * course3Grade;
+totalGradePoints += course4Credit * course4Grade;
+totalGradePoints += course5Credit * course5Grade;
+
+decimal gradePointAverage = (decimal)totalGradePoints / totalCreditHours;
+Console.WriteLine($"Final GPA: {gradePointAverage}");
+
+int leadingDigit = (int)gradePointAverage;
+
+// Para inicializar uma variável que armazenará os dois primeiros dígitos
+// após o separador decimal, insira o seguinte código:
+int firstDigit = (int)(gradePointAverage * 10) % 10;
+int secondDigit = (int)(gradePointAverage * 100) % 10;
+
+// gpa final corrigido
+Console.WriteLine($"\nGpa final corrigido:");
+Console.WriteLine($"Final GPA: {leadingDigit}.{firstDigit}{secondDigit}");
+
+Console.WriteLine($"\nStudent: {studentName}\n");
+
+Console.WriteLine("Course\t\t\tGrade\tCredit Hours");
+
+Console.WriteLine($"{course1Name}\t\t{course1Grade}\t\t{course1Credit}");
+Console.WriteLine($"{course2Name}\t\t{course2Grade}\t\t{course2Credit}");
+Console.WriteLine($"{course3Name}\t\t{course3Grade}\t\t{course3Credit}");
+Console.WriteLine($"{course4Name}\t{course4Grade}\t\t{course4Credit}");
+Console.WriteLine($"{course5Name}\t\t{course5Grade}\t\t{course5Credit}");
+
+Console.WriteLine($"\nFinal GPA:\t\t\t {leadingDigit}.{firstDigit}{secondDigit}");
+
+
+
+
+
+
+
+
+// Console.WriteLine("\nlinha teste");
+// Console.WriteLine($"{totalGradePoints} {totalCreditHours}");
+
+
